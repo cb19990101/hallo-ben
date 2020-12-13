8886ce30d2f384512600e8bfa0a70c6d65d11d2c
cb19990101 <75884741+cb19990101@users.noreply.github.com>
2020-12-13 16:46:33 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.OnceInBackground)
     basic.showString("Hallo Ben!")
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
 })
